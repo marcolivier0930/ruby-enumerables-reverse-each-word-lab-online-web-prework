e4c88adf01d6dbcd6bc949c512fb3d02fa852cf4
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-24 00:48:17 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word(string)
+  string = string.split()
+  reversed_string = []
+  
+  string.each do |char|
+    reversed_string << char.reverse
+  end
+  
+  return reversed_string.join(' ')
+end
+
+
+def reverse_each_word(string)
+  string = string.split()
+  reversed_string = []
+  
+  string.collect do |char|
+    reversed_string << char.reverse
+  end
+  
+  return reversed_string.join(' ')
+end
